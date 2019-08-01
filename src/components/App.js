a7d9035f0da732fe1596a64a9cced9e12061cfba
ruindows\ricar <ricardomalias@gmail.com>
2019-08-01 00:01:55 -0300

    first commit
    Setup of react with redux and materialUI

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+import Button from '@material-ui/core/Button';
+
+// const App = () => (
+//   <div>
+//     <AddTodo />
+//     <VisibleTodoList />
+//     <Footer />
+//   </div>
+// )
+
+const App = () => (
+    <div>
+        
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
+    </div>
+)
+
+export default App
